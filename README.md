f8cdab2fdcacb1ffea13cf77cbdd6fc1560dea35
JazeOu <1710280540@qq.com>
2019-05-15 12:49:47 +0800

    redis后端缓存优化

--- README.md
+++ README.md
@@ -5,19 +5,31 @@
 ![plateform](https://img.shields.io/badge/plateform-windows-lightgrey.svg) 
 ![IntelliJ IDEA](https://img.shields.io/badge/IntelliJ%20IDEA-v2019.1.1-blue.svg) 
 ![JDK](https://img.shields.io/badge/JDK-v1.8.0_121-3A5FCD.svg) 
-![Maven](https://img.shields.io/badge/Maven-v3.6.1-ff69b4.svg) 
-![MySQL](https://img.shields.io/badge/MySQL-v8.0.15-20B2AA.svg) 
+![MySQL](https://img.shields.io/badge/MySQL-v8.0.15-20B2AA.svg)
+![Maven](https://img.shields.io/badge/Maven-v3.6.1-ff69b4.svg)
+![Redis](https://img.shields.io/badge/Redis-3.2.100-20B2AA.svg)
 ![SSM](https://img.shields.io/badge/SSM-framework-yellow.svg) 
 ![build](https://img.shields.io/badge/build-passing-brightgreen.svg) 
 
-本项目使用 SSM 框架开发高并发限时秒杀 web 应用。
+本项目使用 SSM (SpringMVC+Spring+MyBatis) 框架开发高并发限时秒杀 web 应用。
 
-**项目功能介绍：**
+### 功能介绍
 
+- 系统首页是商品列表页，点击秒杀链接进入商品详情页；
+- 进入详情页后，输入手机号进行简单的登录；
 - 商品秒杀开启前，用户能看到商品秒杀倒计时，但不能进行秒杀；
 - 商品秒杀开启时，可以进行秒杀，一个账号对同个商品只能秒杀一次；
 - 商品秒杀结束后，显示商品秒杀已结束，阻止用户进行秒杀。
 
-## 开发进度
-- 已完成：Dao 层和 Service 层
-- 待做：Web层和高并发优化
+### 功能演示
+
+## 特性
+
+- 使用 SSM 框架开发后端业务，采用 RESTful 风格接口设计，并使用 JSON 交互数据；
+- 使用 MD5 混淆秒杀链接，从而防止用户破解秒杀接口提前秒杀；
+- 使用 Redis 缓存热点数据，减少对数据库的访问，提高页面响应时间；
+- 使用存储过程执行秒杀操作，减少数据库行级锁时间，提高 SQL 语句执行速度；
+
+## 改进方向
+
+UI美化，实现比较完整的注册登录功能，增加商户客户端
--- src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -2,6 +2,7 @@ package org.seckill.service.impl;
 
 import org.seckill.dao.SeckillDao;
 import org.seckill.dao.SuccessKilledDao;
+import org.seckill.dao.cache.RedisDao;
 import org.seckill.dto.Exposer;
 import org.seckill.dto.SeckillExecution;
 import org.seckill.entity.Seckill;
@@ -36,6 +37,8 @@ public class SeckillServiceImpl implements SeckillService {
     private SeckillDao seckillDao;
     @Autowired
     private SuccessKilledDao successKilledDao;
+    @Autowired
+    private RedisDao redisDao;
 
     // md5盐值字符串，用于混淆MD5
     private final String slat = "NJLdiufjhn*^*&DFjkjnhfads&^djahjfa:,.";
@@ -52,10 +55,20 @@ public class SeckillServiceImpl implements SeckillService {
 
     @Override
     public Exposer exportSeckillUrl(long seckillId) {
-        Seckill seckill = seckillDao.queryById(seckillId);
+        // 优化点：缓存优化
+        // 1.访问redis
+        Seckill seckill = redisDao.getSeckill(seckillId);
         if (seckill == null) {
-            return new Exposer(false, seckillId);
+            // 2.访问数据库
+            seckill = seckillDao.queryById(seckillId);
+            if (seckill == null) {
+                return new Exposer(false, seckillId);
+            } else {
+                // 3.放入redis
+                redisDao.putSeckill(seckill);
+            }
         }
+
         Date startTime = seckill.getStartTime();
         Date endTime = seckill.getEndTime();
         Date nowTime = new Date();
